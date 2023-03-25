edefa8a48b9f6d825a996299fd98dadd39fb7c33
Chahat1804 <122960416+Chahat1804@users.noreply.github.com>
2023-03-25 14:53:20 +0530

    Update project.py

--- project.py
+++ project.py
@@ -11,20 +11,21 @@ def printBoard(xState, zState):
     six = 'X' if xState[6] else ('O' if zState[6] else 6)
     seven = 'X' if xState[7] else ('O' if zState[7] else 7)
     eight = 'X' if xState[8] else ('O' if zState[8] else 8)
- // position assigning
     print(f"{zero} | {one} | {two} ")
     print(f"--|---|---")
     print(f"{three} | {four} | {five} ")
     print(f"--|---|---")
     print(f"{six} | {seven} | {eight} ") 
+    
 // conditions to win the match
+
 def checkWin(xState, zState):
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for win in wins:
         if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
-            print("X Won the match")
+            print("X Won the match")                                     //when X wins
             return 1
         if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3):
-            print("O Won the match")
+            print("O Won the match")                                      //when O wins
             return 0
     return -1
