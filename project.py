9d6a31053e96347652e49bae4a35a7db627086a5
Chahat1804 <122960416+Chahat1804@users.noreply.github.com>
2023-03-25 18:01:24 +0530

    Update project.py

--- project.py
+++ project.py
@@ -18,14 +18,14 @@ def printBoard(xState, zState):
     print(f"{six} | {seven} | {eight} ") 
 
 def checkWin(xState, zState):
-    // conditions to win the match
+    # conditions to win the match
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for win in wins:
         if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
-            print("X Won the match")                                     //when X wins
+            print("X Won the match")                                     #when X wins
             return 1
         if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3):
-            print("O Won the match")                                      //when O wins
+            print("O Won the match")                                      #when O wins
             return 0
     return -1
 
